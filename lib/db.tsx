1bee2f330fefe6e134cb3d0774b4a61daea352d0
Nicholas Herson <nicholas.herson@gmail.com>
2020-06-06 20:12:55 -0700

    prod service uses real DB

--- lib/db.tsx
+++ lib/db.tsx
@@ -1,14 +1,19 @@
 import {Sequelize} from 'sequelize-typescript';
 
-const sequelize =  new Sequelize({
-        database: 'some_db',
-        dialect: 'sqlite',
-        username: 'root',
-        password: '',
-        storage: ':memory:',
-});
+let sequelize: Sequelize
+if (process.env.ENVIRONMENT === "production") {
+        sequelize = new Sequelize(process.env.DATABASE_URL)
+} else {
+        console.log("using in memory database for storage")
+        sequelize =  new Sequelize({
+                database: 'some_db',
+                dialect: 'sqlite',
+                username: 'root',
+                password: '',
+                storage: ':memory:',
+        });
+}
 
-//sequelize.addModels([Review])
 
 sequelize.sync().then(() => console.log(sequelize.models))
 
