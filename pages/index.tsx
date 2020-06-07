0fad3e5699de6a8537c28a37c96536e9b8968872
Nicholas Herson <nicholas.herson@gmail.com>
2020-06-07 16:14:38 -0700

    client side auth WIP

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,21 +2,27 @@ import Head from 'next/head'
 import Link from 'next/link'
 import Layout from '../components/layout'
 import { Typography } from '@material-ui/core'
+import { withAuth } from 'use-auth0-hooks'
+import React from 'react'
 
 
-export default function Home() {
-  return (
-    <>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+class Home extends React.Component {
+  render() {
+    return (
+      <>
+        <Head>
+          <title>Create Next App</title>
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
 
-      <Layout>
-        <Typography variant="h4" align="center" component="h1" color="textPrimary">
-          Create a <a  href="/reviews/new">review!</a>
-        </Typography>  
-      </Layout>
-    </>
-  )
+        <Layout>
+          <Typography variant="h4" align="center" component="h1" color="textPrimary">
+            Create a <a  href="/reviews/new">review!</a>
+          </Typography>  
+        </Layout>
+      </>
+    )
+  }
 }
+
+export default withAuth(Home)
