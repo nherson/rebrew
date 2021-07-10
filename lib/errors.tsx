6af60c50967c3a286c09f29643a6e2740709fac7
Nicholas Herson <nicholas.herson@gmail.com>
2021-07-10 13:37:02 -0700

    cleanup

--- lib/errors.tsx
+++
@@ -1,7 +0,0 @@
-import { ValidationError } from "sequelize/types";
-import _ from 'lodash';
-
-// format errors into something that looks decent on the API
-export const formatValidationError = (error: ValidationError): string[] => {
-    return _.map(error.errors, (err) => err.message)
-}
